7469235e83327cb5bededa0a691a9b90af614c72
caio <caiomm076@gmail.com>
2025-02-13 16:50:21 -0300

    aula 6

---
+++ aula_6/aula_6/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace aula_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_maca = new System.Windows.Forms.Label();
+            this.pic_maca = new System.Windows.Forms.PictureBox();
+            this.pic_personagem = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_maca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_personagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_maca
+            // 
+            this.lb_maca.AutoSize = true;
+            this.lb_maca.ForeColor = System.Drawing.Color.Lime;
+            this.lb_maca.Location = new System.Drawing.Point(436, 171);
+            this.lb_maca.Name = "lb_maca";
+            this.lb_maca.Size = new System.Drawing.Size(10, 13);
+            this.lb_maca.TabIndex = 2;
+            this.lb_maca.Text = " ";
+            // 
+            // pic_maca
+            // 
+            this.pic_maca.Image = global::aula_6.Properties.Resources.rz3done;
+            this.pic_maca.Location = new System.Drawing.Point(410, 190);
+            this.pic_maca.Name = "pic_maca";
+            this.pic_maca.Size = new System.Drawing.Size(30, 30);
+            this.pic_maca.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_maca.TabIndex = 1;
+            this.pic_maca.TabStop = false;
+            // 
+            // pic_personagem
+            // 
+            this.pic_personagem.Image = global::aula_6.Properties.Resources.character;
+            this.pic_personagem.Location = new System.Drawing.Point(0, 0);
+            this.pic_personagem.Name = "pic_personagem";
+            this.pic_personagem.Size = new System.Drawing.Size(50, 50);
+            this.pic_personagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_personagem.TabIndex = 0;
+            this.pic_personagem.TabStop = false;
+            this.pic_personagem.Click += new System.EventHandler(this.pic_personagem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(714, 441);
+            this.Controls.Add(this.lb_maca);
+            this.Controls.Add(this.pic_maca);
+            this.Controls.Add(this.pic_personagem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_maca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_personagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_personagem;
+        private System.Windows.Forms.PictureBox pic_maca;
+        private System.Windows.Forms.Label lb_maca;
+    }
+}
+
---
+++ aula_6/aula_6/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula_6
+{
+    public partial class Form1 : Form
+    {
+        Personagem p = new Personagem(Velocidade: 15);
+        Maca m = new Maca(Posicao_x: 380,Posicao_y: 160);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += mover;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+            public async void mover(object evento, KeyEventArgs tecla)
+            {
+                switch (tecla.KeyCode)
+                {
+                    case Keys.W:
+                        if (pic_personagem.Top >= 10)
+                        {
+                         pic_personagem.Top -= p.velocidade;
+                        }
+                        break;
+                    case Keys.S:
+                        if (pic_personagem.Top <= 380)
+                        {
+                            pic_personagem.Top += p.velocidade;
+                        }
+                        break;
+                case Keys.A:
+                    if (pic_personagem.Left >= 10)
+                    {
+                        pic_personagem.Left -= p.velocidade;
+                    }
+                    break;
+                case Keys.D:
+                    if (pic_personagem.Left <= 650)
+                    {
+                        pic_personagem.Left += p.velocidade;
+                    }
+                    break;
+                case Keys.E:
+                    int espaco_x = pic_personagem.Left - m.posicao_x;
+                    int espaco_y = pic_personagem.Top - m.posicao_y;
+                    if (espaco_x > 0 && espaco_x < 70 && espaco_y > 0 && espaco_y < 70)
+                    {
+                        pic_maca.Visible = false;
+                        lb_maca.Text = "+ 25 HP";
+                        p.hp += 25;
+                        await Task.Delay(1000);
+                        lb_maca.Visible = false;
+                    }
+                    break;
+                        
+            }
+            }
+        struct Personagem
+        {
+            public int posicao_x;
+            public int posicao_y;
+            public int velocidade;
+            public int hp;
+
+            public Personagem(int Velocidade)
+            {
+                this.posicao_x = 0;
+                this.posicao_y = 0;
+                this.velocidade = Velocidade;
+                this.hp = 180;
+            }
+        }
+
+        public struct Maca
+        {
+            public int posicao_x;
+            public int posicao_y;
+            public int tamanho;
+            public int hp;
+
+            public Maca(int Posicao_x, int Posicao_y)
+            {
+                this.posicao_x = Posicao_x;
+                this.posicao_y = Posicao_y;
+                this.tamanho = 60;
+                this.hp = 25;
+            }
+        }
+
+        private void pic_personagem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
