ddf1ddb17273938bd590a3ad77dfd33df8edfd40
caio <caiomm076@gmail.com>
2025-02-26 15:54:55 -0300

    aula 7

---
+++ aula_7/aula_7/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace aula_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.termos = new System.Windows.Forms.CheckBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.erro_email = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // termos
+            // 
+            this.termos.AutoSize = true;
+            this.termos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.termos.Location = new System.Drawing.Point(63, 224);
+            this.termos.Name = "termos";
+            this.termos.Size = new System.Drawing.Size(162, 17);
+            this.termos.TabIndex = 0;
+            this.termos.Text = "concordo em receber e-mails";
+            this.termos.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(56, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite seu email";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox1.Location = new System.Drawing.Point(63, 147);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(402, 38);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(63, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // erro_email
+            // 
+            this.erro_email.AutoSize = true;
+            this.erro_email.BackColor = System.Drawing.Color.Transparent;
+            this.erro_email.ForeColor = System.Drawing.Color.Red;
+            this.erro_email.Location = new System.Drawing.Point(60, 188);
+            this.erro_email.Name = "erro_email";
+            this.erro_email.Size = new System.Drawing.Size(71, 13);
+            this.erro_email.TabIndex = 7;
+            this.erro_email.Text = "Email invalido";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.erro_email);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.termos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox termos;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label erro_email;
+    }
+}
+
---
+++ aula_7/aula_7/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace aula_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            erro_email.Visible = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string texto = textBox1.Text;
+            string regex = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            erro_email.Visible = false;                 // recebe false antes de verificar a cada mudança
+
+            if (!Regex.IsMatch(texto, regex))
+            {
+                erro_email.Visible = true;
+            }
+       
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string texto = textBox1.Text;
+            bool erro = erro_email.Visible;
+            bool termo = termos.Checked;
+
+            if (termo == true && texto != "" && erro == false)
+            {
+                EnviarEmail();
+            }
+        }
+
+        private void EnviarEmail()
+        {
+            var menssagem = new MimeMessage();
+
+            menssagem.From.Add(new MailboxAddress("caio", RetornarValores("email")));
+            menssagem.To.Add(new MailboxAddress("teste", textBox1.Text));
+            menssagem.Subject = "assunto aqui";
+            menssagem.Body = new TextPart("plain")
+            {
+                Text = "oi"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate(RetornarValores("email"), RetornarValores("senha"));
+                client.Send(menssagem);
+                client.Disconnect(true);
+            }
+        }
+
+        private string RetornarValores(string valores)
+        {
+            if (valores == "email")
+            {
+                return "caiomm076@gmail.com";
+            }
+            if (valores == "senha")
+            {
+                return "gbsz jjpc bzhb esaq";
+            }
+            else
+            return "";
+        }
+    }
+}
+
+
+// conta.de.teste.caio@gmail.com
